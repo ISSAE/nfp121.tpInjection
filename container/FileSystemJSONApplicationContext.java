e18243a907ed460fa41de05bf58ed74ee6948f5c
roygeageaisae <roy.geagea@isae.edu.lb>
2019-03-05 19:00:02 +0200

    minor fixes

--- container/FileSystemJSONApplicationContext.java
+++ container/FileSystemJSONApplicationContext.java
@@ -3,14 +3,17 @@ package container;
 import java.io.*;
 
 public class FileSystemJSONApplicationContext extends AbstractApplicationContext{
+
   private static final boolean T = false; //true; // T comme Trace
-   public String getContainerName(){
+
+  public String getContainerName(){
       return this.getClass().getSimpleName() + " a terminer";
   }
+
   public FileSystemJSONApplicationContext(InputStream inputStream){
     super();
     try{
-      // à compléter
+      // ï¿½ complï¿½ter
       
       
     }catch(Exception e){
